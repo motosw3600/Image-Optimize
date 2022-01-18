50da614317b2834cfa4e776795b3ecd5011beeb2
motosw3600 <motosw3600@gmail.com>
2022-01-18 15:18:30 +0900

    initial Project

---
+++ ImageOptimize/ImageOptimize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageOptimize
+//
+//  Created by 박상우 on 2022/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
