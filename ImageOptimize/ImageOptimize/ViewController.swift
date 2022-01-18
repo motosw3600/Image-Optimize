3db9a1eddfdc9f8e60c167a0f2b9a270c5568718
motosw3600 <motosw3600@gmail.com>
2022-01-18 17:36:18 +0900

    feat: Image downsampling

--- ImageOptimize/ImageOptimize/ViewController.swift
+++ ImageOptimize/ImageOptimize/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func didTouchedRequestButton(_ sender: Any) {
+        guard let path = Bundle.main.path(forResource: "background", ofType: "jpeg") else { return }
+        let url = NSURL(fileURLWithPath: path)
+        self.imageView.image = self.downsampleWithImageIO(url, maxSize: 300)
+    }
+    
+    func downsampleWithImageIO(_ URL:NSURL, maxSize:Float) -> UIImage? {
+        let sourceOptions = [kCGImageSourceShouldCache:false] as CFDictionary
+        guard let source = CGImageSourceCreateWithURL(URL as CFURL, sourceOptions) else { return nil }
+        let downsampleOptions = [
+            kCGImageSourceCreateThumbnailFromImageAlways:true,
+            kCGImageSourceThumbnailMaxPixelSize:maxSize,
+            kCGImageSourceShouldCacheImmediately:true,
+            kCGImageSourceCreateThumbnailWithTransform:true
+        ] as CFDictionary
+        guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(source, 0, downsampleOptions) else {
+            return nil
+        }
+        
+        return UIImage(cgImage: downsampledImage)
+    }
 }
 
